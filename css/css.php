93eff269f1b50c3f8aa5602569449dd02ca56cbe
phucbm <minhphuc1511one@gmail.com>
2018-01-07 22:17:54 +0700

    Add files via upload

---
+++ css/css.php
@@ -0,0 +1,30 @@
+<?php
+echo "
+<!-- jQuery Javascript -->
+<script src='../../CSS/jquery-1.12.4.js'></script>
+
+<!-- Boostrap CSS -->
+<link rel='stylesheet' type='text/css' href='../../CSS/bootstrap.css'>
+
+<!-- Boostrap theme CSS -->
+<link rel='stylesheet' type='text/css' href='../../CSS/bootstrap-theme.css'>
+
+<!-- Boostrap JavaScript -->
+<script src='../../CSS/bootstrap.js'></script>
+
+<!-- BMP Style -->
+<link rel='stylesheet' type='text/css' href='../../CSS/bmp-style.css'>
+
+<!-- Hover CSS -->
+<link rel='stylesheet' type='text/css' href='../../CSS/hover.css'>
+
+<!-- Font Awesome -->
+<link rel='stylesheet' type='text/css' href='../../CSS/font-awesome.css'>
+
+<!-- Icon on Title -->
+<link rel='icon' href='../../Image/favicon.ico' type='image/x-icon'>
+
+<!-- BMP Style 2 -->
+<link rel='stylesheet' type='text/css' href='../../CSS/bmp-style-v2.css'>
+";
+?>
